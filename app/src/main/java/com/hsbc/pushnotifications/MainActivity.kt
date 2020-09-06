123e9f6fa5a041166648bdf2e1e5910ef8934139
swap2705 <swapnilshukla307@gmail.com>
2020-09-06 23:16:35 +0530

    Layout changes: 1. Splash credentials 2. Tab layout

--- app/src/main/java/com/hsbc/pushnotifications/MainActivity.kt
+++ app/src/main/java/com/hsbc/pushnotifications/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hsbc.pushnotifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -12,14 +13,13 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 import com.hsbc.pushnotifications.R
-import com.hsbc.pushnotifications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             // Create channel to show notifications.
@@ -29,6 +29,22 @@ class MainActivity : AppCompatActivity() {
             notificationManager?.createNotificationChannel(NotificationChannel(channelId,
                 channelName, NotificationManager.IMPORTANCE_LOW))
         }
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
 
         // If a notification message is tapped, any data accompanying the notification
         // message is available in the intent extras. In this sample the launcher
@@ -44,10 +60,13 @@ class MainActivity : AppCompatActivity() {
                 val value = intent.extras?.get(key)
                 Log.d(TAG, "Key: $key Value: $value")
             }
+            val intent = Intent(this, TabbedActivity::class.java).apply {
+            }
+            startActivity(intent)
         }
         // [END handle_data_extras]
 
-        binding.subscribeButton.setOnClickListener {
+        /*binding.subscribeButton.setOnClickListener {
             Log.d(TAG, "Subscribing to weather topic")
             // [START subscribe_topics]
             FirebaseMessaging.getInstance().subscribeToTopic("random")
@@ -60,6 +79,28 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
                 }
             // [END subscribe_topics]
+        }
+*/
+
+        /*binding.getStarted.setOnClickListener {
+            FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    val msg = getString(R.string.msg_token_fmt, token)
+                    Log.d(TAG, msg)
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                })
+            val intent = Intent(this, TabbedActivity::class.java).apply {
+            }
+            startActivity(intent)
         }
 
         binding.logTokenButton.setOnClickListener {
@@ -83,7 +124,10 @@ class MainActivity : AppCompatActivity() {
                     instanceTextView.setText(token).toString()
                 })
             // [END retrieve_current_token]
+        }*/
+        val intent = Intent(this, TabbedActivity::class.java).apply {
         }
+        startActivity(intent)
 
     }
 
